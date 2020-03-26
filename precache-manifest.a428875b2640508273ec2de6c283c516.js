70183ff6b9f3173bfc228f49207cf82a6162fc39
Auxy <auxy@Auxys-MacBook-Pro.local>
2020-03-25 21:38:35 -0400

    Updates

---
+++ precache-manifest.a428875b2640508273ec2de6c283c516.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95241c3ab67e1f2b51ff8979136e8a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "338f50fc670b7a5a7830",
+    "url": "/static/js/2.5bb7f735.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.5bb7f735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd74bba93f929312a8e",
+    "url": "/static/js/main.cf55ebe3.chunk.js"
+  },
+  {
+    "revision": "9e567dcd116be2d98b8f",
+    "url": "/static/js/runtime-main.038b38be.js"
+  }
+]);
